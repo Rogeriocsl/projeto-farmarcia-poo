122c83ead5e0b5a8cfc304851b86238d3c635a6a
reginaLeticia <regina.felipe@ccc.ufcg.edu.br>
2019-10-07 09:20:06 -0300

    Algumas documentações

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,29 +1,33 @@
+
+/**
+ * 
+ * Representação de um funcionario da farmacia, que extende de pessoa e possui
+ * um id
+ *
+ * 
+ */
 public class Funcionario extends Pessoa {
 
-    private static int last_id = 0;
+	private static int last_id = 0;
 
-    private int id;
-    private double salario = .0d;
+	private int id;
+	private double salario = .0d;
 
-    public Funcionario(String nome, String cpf)
-    {
-        super(nome, cpf);
+	public Funcionario(String nome, String cpf) {
+		super(nome, cpf);
 
-        this.id = ++last_id;
-    }
+		this.id = ++last_id;
+	}
 
-    public int getId()
-    {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public double getSalario()
-    {
-        return salario;
-    }
+	public double getSalario() {
+		return salario;
+	}
 
-    public void setSalario(double salario)
-    {
-        this.salario = salario;
-    }
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
 }
--- src/Pedido.java
+++ src/Pedido.java
@@ -1,129 +1,118 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * Classe que representa um Produto e quanto ha de quantidade em estoque
+ *
+ * 
+ */
 class ProdutoQuantidade {
 
-    private Produto prod;
-    private int quant;
+	private Produto prod;
+	private int quant;
 
-    public ProdutoQuantidade(Produto prod, int quant)
-    {
-        this.prod = prod;
-        this.quant = quant;
-    }
+	public ProdutoQuantidade(Produto prod, int quant) {
+		this.prod = prod;
+		this.quant = quant;
+	}
 
-    public Produto getProduto()
-    {
-        return prod;
-    }
+	public Produto getProduto() {
+		return prod;
+	}
 
-    public int getQuantidade()
-    {
-        return quant;
-    }
+	public int getQuantidade() {
+		return quant;
+	}
 
-    public void setQuantquantidade(int quant)
-    {
-        this.quant = quant;
-    }
+	public void setQuantquantidade(int quant) {
+		this.quant = quant;
+	}
 }
 
 public class Pedido {
 
-    private static int last_id = 0;
+	private static int last_id = 0;
 
-    private int id;
-    private List<ProdutoQuantidade> produtos;
-    private int idCliente;
-    private int idFuncionario;
+	private int id;
+	private List<ProdutoQuantidade> produtos;
+	private int idCliente;
+	private int idFuncionario;
 
-    public Pedido(int idCliente, int idFuncionario)
-    {
-        this.id = ++last_id;
+	public Pedido(int idCliente, int idFuncionario) {
+		this.id = ++last_id;
 
-        produtos = new ArrayList<>();
+		produtos = new ArrayList<>();
 
-        this.idCliente = idCliente;
-        this.idFuncionario = idFuncionario;
-    }
+		this.idCliente = idCliente;
+		this.idFuncionario = idFuncionario;
+	}
 
-    public int getId()
-    {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public int getIdCliente()
-    {
-        return idCliente;
-    }
+	public int getIdCliente() {
+		return idCliente;
+	}
 
-    public int getIdFuncionario()
-    {
-        return idFuncionario;
-    }
+	public int getIdFuncionario() {
+		return idFuncionario;
+	}
 
-    public double getValor()
-    {
-        double valor = .0d;
+	public double getValor() {
+		double valor = .0d;
 
-        for (ProdutoQuantidade pq : produtos)
-        {
-            valor += pq.getQuantidade() * pq.getProduto().getValor();
-        }
+		for (ProdutoQuantidade pq : produtos) {
+			valor += pq.getQuantidade() * pq.getProduto().getValor();
+		}
 
-        return valor;
-    }
+		return valor;
+	}
 
-    public List<ProdutoQuantidade> getProdutos()
-    {
-        return produtos;
-    }
+	public List<ProdutoQuantidade> getProdutos() {
+		return produtos;
+	}
 
-    public void addProduto(Produto produto, int quant)
-    {
-        ProdutoQuantidade pq = null;
+	public void addProduto(Produto produto, int quant) {
+		ProdutoQuantidade pq = null;
 
-        for (ProdutoQuantidade prodq : produtos)
-        {
-            if (prodq.getProduto().getId() == produto.getId())
-            {
-                pq = prodq;
-                break;
-            }
-        }
+		for (ProdutoQuantidade prodq : produtos) {
+			if (prodq.getProduto().getId() == produto.getId()) {
+				pq = prodq;
+				break;
+			}
+		}
 
-        if (pq == null)
-        {
-            pq = new ProdutoQuantidade(produto, quant);
+		if (pq == null) {
+			pq = new ProdutoQuantidade(produto, quant);
 
-            produtos.add(pq);
-        } else
-        {
-            pq.setQuantquantidade(pq.getQuantidade() + quant);
-        }
-    }
+			produtos.add(pq);
+		} else {
+			pq.setQuantquantidade(pq.getQuantidade() + quant);
+		}
+	}
 
-    /**
-     * Altera a quantidade de determinado produto no pedido.
-     *
-     * @param idProduto Id do Produto especefico.
-     * @param quant nova quantidade
-     * @return true se bem sucedido, false se pedido não contém produto.
-     */
-    public boolean setQuantidadeProduto(int idProduto, int quant)
-    {
-        ProdutoQuantidade prodq = null;
+	/**
+	 * Altera a quantidade de determinado produto no pedido.
+	 *
+	 * @param idProduto Id do Produto especefico.
+	 * @param quant     nova quantidade
+	 * @return true se bem sucedido, false se pedido não contém produto.
+	 */
+	public boolean setQuantidadeProduto(int idProduto, int quant) {
+		ProdutoQuantidade prodq = null;
 
-        for (ProdutoQuantidade pq : produtos)
-        {
-            if (pq.getProduto().getId() == idProduto)
-                prodq = pq;
-        }
+		for (ProdutoQuantidade pq : produtos) {
+			if (pq.getProduto().getId() == idProduto)
+				prodq = pq;
+		}
 
-        if (prodq == null) return false;
+		if (prodq == null)
+			return false;
 
-        prodq.setQuantquantidade(quant);
+		prodq.setQuantquantidade(quant);
 
-        return true;
-    }
+		return true;
+	}
 }
